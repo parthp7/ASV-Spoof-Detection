89733b12f36797233c1fe0be847e7f58f892557a
parth <parthprajapati7995@gmail.com>
2018-03-23 21:53:24 +0530

    window and overlap function added

--- ReadSignal.py
+++ ReadSignal.py
@@ -7,7 +7,47 @@ Created on Fri Mar 23 01:23:31 2018
 """
 import scipy.io.wavfile as wavreader
 from scipy import signal
+import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+
+def pre_emphasis_filter(signal, filter_coeff=0.97):
+    ''' Returns list containing pre-emphasized signal using formula: y[i] = x[i] - a*x[i-1] '''
+    return np.append(signal[0],signal[1:]-filter_coeff*signal[:-1])
+
+
+def rolling_window(sig, window, step):
+    ''' Returns 2-D array from 1-D sig by appling 'window' and 'step' on sig '''
+    shape = sig.shape[:-1] + (sig.shape[-1] - window + 1, window)
+    strides = sig.strides + (sig.strides[-1],)
+    return np.lib.stride_tricks.as_strided(sig, shape=shape, strides=strides)[::step]
+
+
+def hamming_window(length):
+    ''' Returns hamming window function with periodic window'''
+    return signal.hamming(length, False)
+
+
+def window_and_overlap(signal, frame_len, frame_step, winfunc=hamming_window):
+    ''' Apply window function to divide signal into frames '''
+    slen = len(signal)
+    frame_len = int(frame_len)
+    frame_step = int(frame_step)
+    
+    if slen <= frame_len:
+        numframes = 1
+    else:
+        numframes = 1 + int(math.ceil((slen-frame_len)/frame_step))
+        
+    padlen = (numframes-1)*frame_step + frame_len
+    zeros = np.zeros((padlen-slen,))
+    padsig = np.concatenate((signal, zeros))
+    
+    window = winfunc(frame_len)
+    frames = rolling_window(padsig, frame_len, frame_step)
+    
+    return frames*window
 
 
 if __name__ == '__main__':
@@ -22,13 +62,7 @@ if __name__ == '__main__':
     plt.plot(speech_signal)
     
     # Pre-emphasis filter on speech signal
-    # y[i] = x[i] - a*x[i-1]
-    
-    pre_emphasis = [0]
-    alpha = 0.97            # filter coefficient for pre-emphasis
-    
-    for i in range(1,len(speech_signal)):
-        pre_emphasis.append(speech_signal[i]-alpha*speech_signal[i-1])
+    pre_emphasis = pre_emphasis_filter(speech_signal)
         
     # Plot for pre-emphasis filtered signal
     plt.figure(2)
@@ -36,9 +70,7 @@ if __name__ == '__main__':
     
     # Applying Hamming window function
     # Window length 256 and 50% overlap
+    window_len = 256
+    windowed_signal = window_and_overlap(pre_emphasis, window_len, window_len/2)
     
-    win_length = 256
-    window = signal.hamming(win_length, False)
     
-    plt.figure(3)
-    plt.plot(window)
